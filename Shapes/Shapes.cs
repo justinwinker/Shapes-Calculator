d66775ad8c681b743c710a729933ad0251376f74
Justin Winker <justin.winker@trojans.dsu.edu>
2021-09-13 11:31:03 -0500

    minor adjustment

--- Shapes/Shapes.cs
+++ Shapes/Shapes.cs
@@ -11,7 +11,7 @@ namespace Shapes
         public static void ShapeCreation()
         {
                         // Call Shape Selection program
-            Console.WriteLine("What shapes would you like to create?\n[1] Square\n[2] Rectangle\n[3] Circle\n[4] Ellipse\n[5] Pentagon\n\n Press [0] to exit.\n");
+            Console.WriteLine("What shapes would you like to create?\n[1] Square\n[2] Rectangle\n[3] Circle\n[4] Ellipse\n[5] Pentagon\n [6] One of each Shape\n");
             int shapeSelected = int.Parse(Console.ReadLine());
         Console.WriteLine("You entered: {0}\n",shapeSelected);
             switch (shapeSelected)
@@ -22,8 +22,8 @@ namespace Shapes
                     Shapes.ShapeCreation();
                     break;
                 case 2: //Rectangle logic
-                    var rectangle1 = new Rectangle(shapeSelected); //= new Rectangle(shapeSelected);
-                    var rectangle2 = new Rectangle(shapeSelected); //= new Rectangle(shapeSelected);
+                    var rectangle1 = new Rectangle(shapeSelected); 
+                    var rectangle2 = new Rectangle(shapeSelected); 
                     Shapes.ShapeCreation();
                     break;
                 case 3: //Circle logic
@@ -40,6 +40,13 @@ namespace Shapes
                     var pentagon1 = new Pentagon(shapeSelected);
                     var pentagon2 = new Pentagon(shapeSelected);
                     Shapes.ShapeCreation();
+                    break;
+                case 6: //One of each shape
+                    var squareA = new Square(shapeSelected);
+                    var rectangleA = new Rectangle(shapeSelected);
+                    var circleA = new Circle(shapeSelected);
+                    var ellipseA = new Ellipse(shapeSelected);
+                    var pentagonA = new Pentagon(shapeSelected);
                     break;
                 default:
                     Console.WriteLine("You selected an invalid option. Please try again.");
